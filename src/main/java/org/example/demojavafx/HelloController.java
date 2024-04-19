5546f76992d1da88568db32b88fba65297ba37d4
dollemika <dollemika@mail.ru>
2024-04-19 12:05:46 +0300

    Initial commit

---
+++ src/main/java/org/example/demojavafx/HelloController.java
@@ -0,0 +1,39 @@
+package org.example.demojavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    Label label1;
+    @FXML
+    Label label2;
+    @FXML
+    Label area;
+    @FXML
+    TextField textx;
+    @FXML
+    TextField texty;
+    public void doPlus(){
+        label1.setText("Сложение");
+        label2.setText(String.valueOf(Integer.parseInt(textx.getText())+Integer.parseInt(texty.getText())));
+    }
+    public void doMinus(){
+        label1.setText("Вычитание");
+        label2.setText(String.valueOf(Integer.parseInt(textx.getText())-Integer.parseInt(texty.getText())));
+    }
+    public void sqArea(){
+        label1.setText("Площадь прямоугольника");
+        int x = Integer.parseInt(textx.getText());
+        int y = Integer.parseInt(texty.getText());
+        try {
+            Rectangle r = new Rectangle(x, y);
+            label2.setText(String.valueOf(r.area()));
+        }
+        catch (Exception e){
+            label2.setText("Стороны не подходят");
+        }
+    }
+
+}
